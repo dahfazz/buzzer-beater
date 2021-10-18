d7594a8d96ae49f56cab6d7f76328dc54acb8d9a
fabien zibi <fabien.zibi@gmail.com>
2021-10-18 11:10:46 +0200

    get ready for heroku

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('App listening', port);
+});
 
 const dateFormat = require('dateformat');
 const axios = require('axios');
