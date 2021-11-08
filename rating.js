05e36e0479cf0872954ca9934f248e732671c0dd
fabien zibi <fabien.zibi@gmail.com>
2021-11-08 16:43:12 +0100

    fix rating

--- rating.js
+++ rating.js
@@ -3,12 +3,9 @@ const STANDINGS = require('./STANDINGS.json');
 module.exports = (data) => {
   let score = 100;
 
-  const delta = data.delta;
-  const teamA = data.teamA;
-  const teamB = data.teamB;
+  const delta = data.delta || 0;
   const standA = STANDINGS.filter(st => st.team === data.teamA)[0].rank;
   const standB = STANDINGS.filter(st => st.team === data.teamB)[0].rank;
-  const OT = data.ot;
 
   score -= delta;
   score -= standA;
