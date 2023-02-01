e3fcdd70fc1e97aae37e320e53268b84e77b0b12
fabien zibi <fabien.zibi@gmail.com>
2023-02-01 18:29:02 +0100

    blocked

--- routes/ttfl.js
+++ routes/ttfl.js
@@ -49,7 +49,7 @@ module.exports = async (req, res) => {
     <body>
     <div class="container g-0">
     <div class="row justify-content-center">
-    <div class="col col-lg-4">
+    <div class="col col-lg-6">
     <ul class="list-group">`
 
 
@@ -57,19 +57,77 @@ module.exports = async (req, res) => {
       const injuryStatus = getInjuryStatus(line.player);
       html += `<li style="`;
 
-      html += index % 2 ? '' : 'background: rgba(0, 0, 0, .1)'
+      html += index % 2 ? '' : 'background: rgba(0, 0, 0, .05)'
 
-      html += `" class="list-group-item d-flex justify-content-between"><div><span>${line.player}</span> (${line.team_id} vs ${nightGames.oppositions[line.team_id]})`
+      html += `" class="list-group-item align-items-center d-flex justify-content-between"><div><span id="player-${line.player.replaceAll(' ', '-')}" class="fw-bold">${line.player}</span> (${line.team_id} vs ${nightGames.oppositions[line.team_id]})`
 
       if (injuryStatus) {
         html += `<span class="ms-2 badge bg-danger">${injuryStatus.status}</span>`
       }
 
-      html += `</div> <span class="badge bg-primary">${parseInt(line.TTFL, 10)}</span></li>\n`
+
+
+      html += `</div>`
+
+      html += `
+      <div class="d-flex justify-content-end"><select data-player="${line.player}" class="me-2 form-select select-blocking-days">
+      <option value="">bloqué ?</option>`
+
+      Array.from(Array(30).keys()).forEach(nb => {
+
+        html += `<option value = "${nb}" > ${nb}</option>`
+      })
+
+      html += `</select>
+      <span class="badge bg-primary d-flex align-items-center">${parseInt(line.TTFL, 10)}</span></div></li>\n`
     })
     html += `</ul></div>
     </div>
-    </div></body>
+    </div>
+    <script>
+      Date.prototype.addDays = function(days) {
+        var date = new Date(this.valueOf());
+        date.setDate(date.getDate() + days);
+        return date;
+      }
+
+      const daysBetween = (t1, t2) => (t2 - t1) / (1000 * 3600 * 24)
+
+      const selects = Array.from(document.querySelectorAll('.select-blocking-days'))
+      selects.forEach(select => {
+        select.addEventListener('change', (e) => {
+          const el = e.target;
+          const player = el.getAttribute('data-player')
+          const days = parseInt(el.value, 10);
+
+          const today = new Date()
+          const end = today.addDays(days)
+          window.localStorage.setItem(player, end.getTime());
+        })
+      })
+
+      selects.forEach(select => {
+        const player = select.getAttribute('data-player')
+        const storage = parseInt(window.localStorage.getItem(player), 10)
+        const today = new Date().getTime()
+        const isInFuture = storage && storage > today;
+
+        if (storage) {
+          if (isInFuture) {
+            const nameEl = document.querySelector('#player-' + player.replaceAll(' ', '-'));
+            if (nameEl){
+              nameEl.classList.remove('fw-bold')
+              nameEl.classList.add('text-secondary')
+              nameEl.classList.add('fw-light')
+            }
+            select.value = Math.round(daysBetween(today, storage));
+          } else {
+            window.localStorage.removeItem(player)
+          }
+        }
+      })
+    </script>
+    </body>
   </html>`;
 
     return res.send(html);
